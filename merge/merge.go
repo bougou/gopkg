03ea5a852f2b40718e888530751fcce8f3842c1b
Bougou <bougou@126.com>
2021-09-15 19:14:39 +0800

    Add map merge method

---
+++ merge/merge.go
@@ -0,0 +1,64 @@
+package merge
+
+type Map struct {
+	items map[string]interface{}
+
+	// Whether to use src field value to overrite the dst field value.
+	// Default false.
+	overwrite bool
+
+	// Whether to add the src field to the dst if the field does not exist in the dst.
+	// Default true.
+	newField bool
+}
+
+func NewMap(m map[string]interface{}) *Map {
+	return &Map{
+		items: m,
+
+		overwrite: false,
+		newField:  true,
+	}
+}
+
+type Option func(m *Map) *Map
+
+func WithOverride(m *Map) *Map {
+	m.overwrite = true
+	return m
+}
+
+func WithNoNewField(m *Map) *Map {
+	m.newField = false
+	return m
+}
+
+func (m *Map) Merge(in map[string]interface{}, options ...Option) error {
+	for _, option := range options {
+		option(m)
+	}
+
+	for k, v := range in {
+		_, exists := m.items[k]
+		if !exists && m.newField {
+			m.items[k] = v
+			continue
+		}
+
+		srcValue, ok1 := v.(map[string]interface{})
+		dstValue, ok2 := m.items[k].(map[string]interface{})
+		if ok1 && ok2 {
+			mm := NewMap(dstValue)
+			if err := mm.Merge(srcValue, options...); err != nil {
+				return err
+			}
+			continue
+		}
+
+		if m.overwrite {
+			m.items[k] = v
+		}
+	}
+
+	return nil
+}
---
+++ merge/merge_test.go
@@ -0,0 +1,46 @@
+package merge
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/bougou/gopkg/common"
+)
+
+func Test_merge(t *testing.T) {
+	tests := []struct {
+		src map[string]interface{}
+		dst map[string]interface{}
+	}{
+
+		{
+			src: map[string]interface{}{
+				"a": map[string]interface{}{
+					"k": "hello",
+				},
+				"b": "some",
+				"c": 12,
+				"d": map[string]interface{}{
+					"e": "test",
+				},
+			},
+
+			dst: map[string]interface{}{
+				"a": 100,
+				"c": "hello",
+				"d": map[string]interface{}{
+					"e": "jump",
+					"f": "hi",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		m := NewMap(tt.dst)
+		m.Merge(tt.src, WithOverride)
+		b, _ := common.Encode("json", tt.dst)
+		fmt.Println(string(b))
+	}
+
+}
