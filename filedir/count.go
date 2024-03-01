50e1b92ce1da16c08f23455f7045ef1c3a74b998
Bougou <bougou@126.com>
2024-03-01 19:19:31 +0800

    x

---
+++ filedir/count.go
@@ -0,0 +1,44 @@
+package filedir
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func CountItemsOfDir(dirPath string) (int, error) {
+	file, err := os.Open(dirPath)
+	if err != nil {
+		return 0, fmt.Errorf("open dir %s failed, err: %s", dirPath, err)
+	}
+
+	files, err := file.Readdirnames(-1)
+	if err != nil {
+		return 0, err
+	}
+
+	return len(files), nil
+}
+
+func CountItemsOfDirRecusively(dirPath string) (int, error) {
+	count := 0
+
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("Error accessing path %q: %v\n", path, err)
+			return nil
+		}
+
+		// Increment the count for each item
+		count++
+		return nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	// Subtract 1 from the count to exclude the root directory itself
+	count--
+	return count, nil
+}
